4a100b462d631ebafc70c6757e728d1578c99db8
Daniel Le <GreenRecycleBin@gmail.com>
2014-01-02 20:51:58 +0800

    Layout the widgets

--- GeoQuiz/src/main/java/com/greenrecyclebin/android/geoquiz/QuizActivity.java
+++ GeoQuiz/src/main/java/com/greenrecyclebin/android/geoquiz/QuizActivity.java
@@ -1,15 +1,13 @@
 package com.greenrecyclebin.android.geoquiz;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
 public class QuizActivity extends ActionBarActivity {
 
@@ -17,18 +15,11 @@ public class QuizActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
-
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.quiz, menu);
         return true;
@@ -56,7 +47,7 @@ public class QuizActivity extends ActionBarActivity {
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
+                                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
         }
