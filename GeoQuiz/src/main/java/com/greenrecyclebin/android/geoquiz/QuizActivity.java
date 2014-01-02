397e9220efb6c403c2cb70e2e396d3b15c6acd23
Daniel Le <GreenRecycleBin@gmail.com>
2014-01-02 21:02:09 +0800

    Getting references to the buttons

--- GeoQuiz/src/main/java/com/greenrecyclebin/android/geoquiz/QuizActivity.java
+++ GeoQuiz/src/main/java/com/greenrecyclebin/android/geoquiz/QuizActivity.java
@@ -11,10 +11,16 @@ import android.view.ViewGroup;
 
 public class QuizActivity extends ActionBarActivity {
 
+    private Button mTrueButton;
+    private Button mFalseButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalseButton = (Button) findViewById(R.id.false_button);
     }
 
     @Override
