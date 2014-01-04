159435d5379442ec71bf2fb7d22f2ff0244d5e81
Daniel Le <GreenRecycleBin@gmail.com>
2014-01-04 13:10:41 +0800

    Wire up the TextView

--- GeoQuiz/src/main/java/com/greenrecyclebin/android/geoquiz/QuizActivity.java
+++ GeoQuiz/src/main/java/com/greenrecyclebin/android/geoquiz/QuizActivity.java
@@ -33,6 +33,10 @@ public class QuizActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        int question = mQuestionBank[mCurrentIndex].getQuestion();
+        mQuestionTextView.setText(question);
+
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
